12c29c1cb321730d1fcf40354f6982d165c34886
Silvestre <ssivasquez@gmail.com>
2020-12-24 12:06:08 -0600

    REST Server - Usuario

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
 
 
-mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, (err, res) =>{
+mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useCreateIndex: true}, (err, res) =>{
 
     if(err) throw err;
 
